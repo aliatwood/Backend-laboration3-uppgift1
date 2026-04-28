0adb5020ea300224cbbac217229a267fe88fc784
aliatwood <aliatwood0104@gmail.com>
2026-04-28 23:14:40 +0200

    Skapade server.js
    Skapade Express server och kopplade MongoDB samt routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const workExperienceRoutes = require("./workExperience");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.use("/api/workexperience", workExperienceRoutes);
+
+app.listen(3000);
